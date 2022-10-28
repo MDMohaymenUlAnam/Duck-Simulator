32d4f92bc90f7cebd55ebebc6dec61b52e390298
MDMohaymenUlAnam <78275354+MDMohaymenUlAnam@users.noreply.github.com>
2022-10-29 00:05:55 +0600

    created the duck classes

---
+++ src/com/company/RedHeadDuck.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class RedHeadDuck implements Quackable {
+    public void quack() {
+        System.out.println("Quack");
+    }
+}
---
+++ src/com/company/RubberDuck.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class RubberDuck implements Quackable {
+    public void quack() {
+        System.out.println("Squeak");
+    }
+}
